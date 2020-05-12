99a32536f6ee7e753322fc9051cb59d75782b3f8
Pedro Guimaraes <pedromonteirogui@gmail.com>
2020-05-11 21:25:50 -0300

    List exercise

---
+++ listasSeção6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace listasSeção6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trabalhador> list = new List<Trabalhador>();
+
+            Console.WriteLine("Quantos empregados serão registrados?");
+            int qteEmpregados = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qteEmpregados; i++)
+            {
+                Console.Write("Informe o Id do empregado: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Informe o nome do empregado: ");
+                string nome = Console.ReadLine();
+                Console.Write("Informe o salario do empregado: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Trabalhador(id, nome, salario));
+            }
+
+            foreach (Trabalhador trab in list)
+            {
+                Console.WriteLine(trab);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine();
+            Console.Write("Entre com o id do empregado que tera aumento do salario: ");
+            int idAumento = int.Parse(Console.ReadLine());
+            Console.Write("Entre com a porcentagem de aumento: ");
+            int porcAumento = int.Parse(Console.ReadLine());
+
+            Trabalhador trabAumento = list.Find(x => x.Id == idAumento);
+
+            trabAumento.AumentarSalario(porcAumento);
+
+            foreach (Trabalhador trab in list)
+            {
+                Console.WriteLine(trab);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ listasSeção6/Trabalhador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace listasSeção6
+{
+    class Trabalhador
+    {
+        public int Id;
+        public string Nome;
+        public double Salario;
+
+        public Trabalhador(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(int porcentAumento)
+        {
+            Salario = Salario + Salario * porcentAumento / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
